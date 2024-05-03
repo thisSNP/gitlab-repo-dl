ffe82d1f9cffb9e87efa2bb77ee33a97e7e14d0f
Sebastian Pils <37801716+thisSNP@users.noreply.github.com>
2024-05-03 22:36:27 +0200

    add download everything functionalty

--- gitlab-repo-dl.sh
+++ gitlab-repo-dl.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+if [ "$1" == "all" ]; then
+    GITLAB_URL="set me"
+    GITLAB_TOKEN="set me"
+fi
+
+if [ "$1" == "all-in-one" ]; then
+    GITLAB_URL="set me"
+    GITLAB_TOKEN="set me"
+fi
+
 if [ -z "$GITLAB_URL" ]; then
     echo "Missing environment variable: GITLAB_URL (e.g. https://gitlab.com)"
     exit 1
@@ -26,10 +36,10 @@ if [ "$1" == "group" ]; then
 
     echo "Cloning all git projects in group $GROUP_NAME"
 
-    REPO_SSH_URLS=$(curl -s "$GITLAB_URL/api/v4/groups/$GROUP_NAME/projects?private_token=$GITLAB_TOKEN&per_page=999" | jq '.[] | .ssh_url_to_repo' | sed 's/"//g')
+    REPO_SSH_URLS=$(curl -s "$GITLAB_URL/api/v4/groups/$GROUP_NAME/projects?include_subgroups=true&private_token=$GITLAB_TOKEN&per_page=999" | jq '.[] | .ssh_url_to_repo' | sed 's/"//g')
 
     for REPO_SSH_URL in $REPO_SSH_URLS; do
-        REPO_PATH="$GROUP_NAME/$(echo "$REPO_SSH_URL" | awk -F'/' '{print $NF}' | awk -F'.' '{print $1}')"
+        REPO_PATH="$GROUP_NAME/$(echo "$REPO_SSH_URL" | awk -F'/' '{print $NF}' | awk -F'.' '{print $1}{print $2}')"
 
         if [ ! -d "$REPO_PATH" ]; then
             echo "git clone $REPO_PATH"
@@ -41,11 +51,11 @@ if [ "$1" == "group" ]; then
     done
 elif [ "$1" == "all-repo-list" ]; then
     # Get total number of pages (with 20 projects per page) from HTTP header
-    TOTAL_PAGES=`curl "$GITLAB_URL/api/v4/projects?private_token=$GITLAB_TOKEN" -sI | grep X-Total-Pages | awk '{print $2}' | sed 's/\\r//g'`
+    TOTAL_PAGES=$(curl "$GITLAB_URL/api/v4/projects?include_subgroups=true&private_token=$GITLAB_TOKEN" -sI | grep X-Total-Pages | awk '{print $2}' | sed 's/\\r//g')
 
     for ((PAGE_NUMBER = 1; PAGE_NUMBER <= TOTAL_PAGES; PAGE_NUMBER++)); do
         # echo git@instance:namespace/repo.git
-        curl "$GITLAB_URL/api/v4/projects?private_token=$GITLAB_TOKEN&per_page=20&page=$PAGE_NUMBER" | jq '.[] | .ssh_url_to_repo' | sed 's/"//g'
+        curl "$GITLAB_URL/api/v4/projects?include_subgroups=true&private_token=$GITLAB_TOKEN&per_page=20&page=$PAGE_NUMBER" | jq '.[] | .ssh_url_to_repo' | sed 's/"//g'
     done
 elif [ "$1" == "from-list" ]; then
     if [ -z "$2" ]; then
@@ -76,4 +86,67 @@ elif [ "$1" == "from-list" ]; then
             (cd "$TARGET_DIR/$REPO_PATH" && git pull)
         fi
     done <"$LIST_FILE"
+
+
+## downlad everything
+elif [ "$1" == "all" ]; then
+
+    GITLAB_URL="$2"
+    GITLAB_TOKEN="$3"
+
+    MYGROUPS=$(curl -s "$GITLAB_URL/api/v4/groups/?private_token=$GITLAB_TOKEN&per_page=999" | jq '.[].path' | sed 's/"//g')
+
+    for CURRENT_GROUP in $MYGROUPS; do
+        REPO_SSH_URLS=$(curl -s "$GITLAB_URL/api/v4/groups/$CURRENT_GROUP/projects?include_subgroups=true&private_token=$GITLAB_TOKEN&per_page=999" | jq '.[] | .ssh_url_to_repo' | sed 's/"//g')
+        
+        for u in $REPO_SSH_URLS; do
+            
+            echo "$u"            
+            read -a parts <<< "$u"
+            
+            REPO_PATH="$(echo "$parts" | awk -F':' '{print $2}' | awk -F'.' '{print $1}' | awk -F'/' '{print $1 "-" $2}' )"
+            echo "$REPO_PATH"
+
+            if [ ! -d "$REPO_PATH" ]; then
+                echo "git clone $u"
+                git clone "$u" "$REPO_PATH"
+            else
+                echo "git pull $REPO_PATH"
+                (cd "$REPO_PATH" && git pull)
+            fi
+        done
+    done
+
+elif [ "$1" == "all-in-one" ]; then
+
+    GITLAB_URL="$2"
+    GITLAB_TOKEN="$3"
+
+    MYGROUPS=$(curl -s "$GITLAB_URL/api/v4/groups/?private_token=$GITLAB_TOKEN&per_page=999" | jq '.[].path' | sed 's/"//g')
+
+    for CURRENT_GROUP in $MYGROUPS; do
+
+        if [ ! -d "$CURRENT_GROUP" ]; then
+            mkdir "$CURRENT_GROUP"
+            cd "$CURRENT_GROUP" || exit
+        else
+            cd "$CURRENT_GROUP" || exit
+        fi
+
+        REPO_SSH_URLS=$(curl -s "$GITLAB_URL/api/v4/groups/$CURRENT_GROUP/projects?include_subgroups=true&private_token=$GITLAB_TOKEN&per_page=999" | jq '.[] | .ssh_url_to_repo' | sed 's/"//g')
+
+        for REPO_SSH_URL in $REPO_SSH_URLS; do
+            REPO_PATH="$(echo "$REPO_SSH_URL" | awk -F'/' '{print $NF}' | awk -F'.' '{print $1}{print $2}')"
+
+            if [ ! -d "$REPO_PATH" ]; then
+                echo "git clone $REPO_PATH"
+                git clone "$REPO_SSH_URL" "$REPO_PATH"
+            else
+                echo "git pull $REPO_PATH"
+                (cd "$REPO_PATH" && git pull)
+            fi
+        done
+        cd ..
+    done
 fi
+
